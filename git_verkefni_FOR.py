d63e44ea6a38d8f15c06199a40f72b0fc77289d6
User218 <alexanderem218@nemi.tskoli.is>
2017-01-25 11:02:48 +0000

    Dæmi 1

--- git_verkefni_FOR.py
+++ git_verkefni_FOR.py
@@ -1,1 +1,8 @@
 #Git verkefni FOR
+#Alexander Örn
+#25/01/17
+
+#Dæmi 1
+t1=int(input("Sláðu inn tölu: "))
+t2=int(input("Sláðu inn aðra tölu: "))
+print("Tölurnar lagðar saman eru",t1+t2,"og tölurnar margfaldaðar saman eru",t1*t2)
